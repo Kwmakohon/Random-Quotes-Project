4590932747216c62761ffd381a1cd00459102f66
Kristen Makohon <kristen@Kristens-MBP.attlocal.net>
2020-04-22 17:30:34 -0400

    Made my own code

--- js/app.js
+++ js/app.js
@@ -1,44 +1,40 @@
-//immediate invoked function expression
-(function() {
-  const quotes = [
-    {
-      quote:
-        "Life is too short and sweet to be spent by cribbing and complaining about things. Here are some random quotes about the most wonderful gift that we've got",
-      author: " Life"
-    },
-    {
-      quote:
-        "Humor is richly rewarding to the person who employs it. It has some value in gaining and holding attention. But it has no persuasive value at all",
-      author: "John Kenneth Galbraith"
-    },
-    {
-      quote:
-        "God save me from my friends. I can protect myself from my enemies.",
-      author: "Claude Louis Hector de Villars "
-    },
-    {
-      quote: "The price of anything is the amount of life you exchange for it.",
-      author: "David Thoreau"
-    },
-    {
-      quote:
-        "Life is like a landscape. You live in the midst of it but can describe it only from the vantage point of distance. ",
-      author: "Charles Lindbergh"
-    },
-    {
-      quote:
-        "A critic is someone who never actually goes to the battle, yet who afterwards comes out shooting the wounded.",
-      author: " Tyne Daly"
-    }
-  ];
 
-  const btn = document.getElementById("generate-btn");
 
-  btn.addEventListener("click", function() {
-    let random = Math.floor(Math.random() * quotes.length);
-    console.log(random);
+const quotes = [
+  {
+    quote: 'Be yourself; everyone else is already taken."',
+    author: 'Oscar Wilde'
+  },
+  {
+    quote: `Two things are infinite: the universe and human stupidity; and I'm not sure about the universe.”`, 
+    author: 'Albert Einstein'
+  },
+  {
+    quote:'You only live once, but if you do it right, once is enough."',
+    author: 'Mae West'
+  },
+  {
+    quote: `In three words I can sum up everything I've learned about life: it goes on."`,
+    author: 'Robert Frost'
+  },
+  {
+    quote: 'Always forgive your enemies; nothing annoys them so much.”',
+    author: 'Oscar Wilde'
+  },
+  {
+    quote: `I've learned that people will forget what you said, people will forget what you did, but people will never forget how you made them feel."`,
+    author: 'Maya Angelou'
+  }
+];
 
-    document.getElementById("quote").textContent = quotes[random].quote;
-    document.querySelector(".author").textContent = quotes[random].author;
-  });
-})();
+const random = () => Math.floor(Math.random() * quotes.length);
+
+let btn = document.getElementById('generate-btn');
+const q = document.getElementById('quote');
+const a = document.getElementById('author');
+
+btn.addEventListener("click", () => {
+  const num = random()
+  q.textContent = quotes[num].quote;
+  a.textContent = quotes[num].author;
+});
